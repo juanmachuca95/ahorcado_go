4e834d6d6efdba9ffff52613dec71ec71040e38a
Machuca Juan <machucajuangabriel@gmail.com>
2022-05-08 17:47:37 -0300

    first commit

---
+++ cmd/main/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	log.Println("Todo okay -- arrancamos!.")
+
+	s := ` 
+	   _____
+	   |   |
+	  _O/  |
+	   |   |
+	  / \  |
+	 ______|
+	 `
+
+	fmt.Println(s)
+}
---
+++ frames/frames.go
@@ -0,0 +1,99 @@
+package frames
+
+import "fmt"
+
+func inicial() {
+	s := ` 
+		   _____
+		   |   |
+		  _O/  |
+		   |   |
+		  / \  |
+		 ______|
+		 `
+	fmt.Print(s)
+}
+
+func first_attempt() {
+	s := ` 
+		   _____
+		   |   |
+		  _O/  |
+		   |   |
+		    \  |
+		 ______|
+		 `
+	fmt.Print(s)
+}
+
+func second_attempt() {
+	s := ` 
+		   _____
+		   |   |
+		  _O/  |
+		   |   |
+		       |
+		 ______|
+		 `
+	fmt.Print(s)
+}
+
+func third_attempt() {
+	s := ` 
+		   _____
+		   |   |
+		  _O/  |
+		       |
+		       |
+		 ______|
+		 `
+	fmt.Print(s)
+}
+
+func fourth_attempt() {
+	s := ` 
+		   _____
+		   |   |
+		  _O   |
+		       |
+		       |
+		 ______|
+		 `
+	fmt.Print(s)
+}
+
+func fifth_attempt() {
+	s := ` 
+		   _____
+		   |   |
+		  _O   |
+		       |
+		       |
+		 ______|
+		 `
+	fmt.Print(s)
+}
+
+func sixth_attempt() {
+	s := ` 
+		   _____
+		   |   |
+		   O   |
+		       |
+		       |
+		 ______|
+		 `
+	fmt.Print(s)
+}
+
+func seventh_attempt() {
+	s := ` 
+		   _____
+		   |   |
+		       |
+		       |
+		       |
+		 ______|
+		 `
+	fmt.Print(s)
+}
