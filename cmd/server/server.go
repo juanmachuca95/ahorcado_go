c646cf8ae9c9f1c86fa96e9871a6d63ee8e9e4b8
Machuca Juan Gabriel <machucajuangabriel@gmail.com>
2022-08-14 20:56:00 -0300

    test

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/joho/godotenv"
@@ -15,14 +18,34 @@ import (
 	ah "github.com/juanmachuca95/ahorcado_go/protos/ahorcado"
 	au "github.com/juanmachuca95/ahorcado_go/protos/auth"
 	"github.com/rs/cors"
+	"github.com/sirupsen/logrus"
 	"github.com/tmc/grpc-websocket-proxy/wsproxy"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/reflection"
 )
+
+var (
+	KeyPair  *tls.Certificate
+	CertPool *x509.CertPool
+	logger   *logrus.Logger
+)
+
+func init() {
+	logger = logrus.StandardLogger()
+	logrus.SetLevel(logrus.InfoLevel)
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors:     true,
+		FullTimestamp:   true,
+		TimestampFormat: time.Kitchen,
+		DisableSorting:  true,
+	})
+	grpclog.SetLogger(logger)
+}
 
 func LoadTLSCredentials() (credentials.TransportCredentials, error) {
 	// load server's certificate and private key
@@ -31,9 +54,22 @@ func LoadTLSCredentials() (credentials.TransportCredentials, error) {
 		return nil, err
 	}
 
+	// read ca's cert, verify to client's certificate
+	caPem, err := ioutil.ReadFile("cert/ca-cert.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create cert pool and append ca's cert
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(caPem) {
+		log.Fatal(err)
+	}
+
 	config := &tls.Config{
 		Certificates: []tls.Certificate{serverCert},
-		ClientAuth:   tls.NoClientCert,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    certPool,
 	}
 
 	return credentials.NewTLS(config), nil
@@ -58,6 +94,8 @@ func main() {
 
 	// Middleware
 	authInterceptor := interceptor.NewAuthInterceptor()
+
+	// GRPC Server
 	servGrpc := grpc.NewServer(
 		grpc.Creds(tlsCredentials),
 		grpc.UnaryInterceptor(authInterceptor.UnaryInterceptor()),
@@ -71,16 +109,13 @@ func main() {
 	// Enable reflection
 	reflection.Register(servGrpc)
 
-	conn, err := grpc.DialContext(
-		ctx,
-		"0.0.0.0:8080",
-		grpc.WithTransportCredentials(tlsCredentials),
-	)
+	conn, err := grpc.DialContext(ctx, "0.0.0.0:8080", grpc.WithTransportCredentials(tlsCredentials))
 	// conn, err := grpc.DialContext(ctx, "0.0.0.0:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalln("Failed to dial server:", err)
 	}
 
+	// REST Server
 	gwmux := runtime.NewServeMux()
 	// Register AhorcadoHandler
 	if err := ah.RegisterAhorcadoHandler(context.Background(), gwmux, conn); err != nil {
@@ -97,14 +132,16 @@ func main() {
 	}
 
 	log.Println("Serving gRPC & gRPC-Gateway on 0.0.0.0:8080")
-	log.Fatalln(http.ListenAndServe(":8080", grpcHandlerFunc(servGrpc, gwServer.Handler)))
+	log.Fatalln(http.ListenAndServeTLS(":8080", "cert/server-cert.pem", "cert/server-key.pem", grpcHandlerFunc(servGrpc, gwServer.Handler)))
 }
 
 func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
 	return h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+			log.Println("GRPC")
 			grpcServer.ServeHTTP(w, r)
 		} else {
+			log.Println("REST")
 			otherHandler.ServeHTTP(w, r)
 		}
 	}), &http2.Server{})
